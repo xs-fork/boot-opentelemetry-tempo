a77a3a70aa4f75d4be8b59cd6d66d4399ec306ce
王海奇 <haiqi.wang@clickpaas.com>
2022-03-29 17:40:06 +0800

    :bug: fix

--- boot-otel-tempo-provider1/src/main/java/io/opentelemetry/example/flight/FlightService.java
+++ boot-otel-tempo-provider1/src/main/java/io/opentelemetry/example/flight/FlightService.java
@@ -1,15 +1,10 @@
 package io.opentelemetry.example.flight;
 
+import io.opentelemetry.example.flight.repository.FlightRepository;
+import io.opentelemetry.example.flight.repository.entity.Flight;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-import io.opentelemetry.api.common.AttributeKey;
-import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.example.flight.repository.FlightRepository;
-import io.opentelemetry.example.flight.repository.entity.Flight;
-import io.opentelemetry.extension.annotations.WithSpan;
 
 @Service
 public class FlightService {
@@ -23,22 +18,22 @@ public class FlightService {
 	}
 
 	public Iterable<Flight> getFlights(String origin) {
-		doSomeWorkNewSpan();
+//		doSomeWorkNewSpan();
 		return flightRepository.findAll();
 	}
 
-	@WithSpan
-    public void doSomeWorkNewSpan() {
-		LOGGER.info("Doing some work In New span");
-        Span span = Span.current();
- 
-        span.setAttribute("template.a2", "some value");
- 
-        span.addEvent("template.processing2.start", atttributes("321"));
-        span.addEvent("template.processing2.end", atttributes("321"));
-    }
- 
-    private Attributes atttributes(String id) {
-        return Attributes.of(AttributeKey.stringKey("app.id"), id);
-    }
+//	@WithSpan
+//    public void doSomeWorkNewSpan() {
+//		LOGGER.info("Doing some work In New span");
+//        Span span = Span.current();
+//
+//        span.setAttribute("template.a2", "some value");
+//
+//        span.addEvent("template.processing2.start", atttributes("321"));
+//        span.addEvent("template.processing2.end", atttributes("321"));
+//    }
+//
+//    private Attributes atttributes(String id) {
+//        return Attributes.of(AttributeKey.stringKey("app.id"), id);
+//    }
 }
