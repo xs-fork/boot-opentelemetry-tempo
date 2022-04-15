89862aea69f42916e3175cd84f0dde9eb7239d18
haiqi.wang <haiqi.wang@ly.com>
2022-04-15 17:15:17 +0800

    fix

---
+++ docker-compose/start-tempo-skywalking.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+bridge_name='xs-bridge'
+bridge="$(docker network ls | grep $bridge_name)"
+if [ -z "$bridge" ]; then
+  docker network create -d bridge "$bridge_name"
+fi
+docker-compose -f docker-compose-tempo-skywalking.yaml up
